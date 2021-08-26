7f3235ce2dd2ef3b68bf9781c42c4205f2ac0010
emilyemiller <36715396+emilyemiller@users.noreply.github.com>
2021-08-26 08:42:38 -0400

    Add files via upload

---
+++ pullFK21.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os
+
+cruise_id = 'FK210409'
+os.chdir('/Users/atlanticindianocean/Desktop/scratch/mgds/work/Falkor/'+cruise_id+'/Subastian')
+
+dive_ids = []
+paths2jsons=[]
+for i in range(len(os.listdir())):
+    dive = os.listdir()[i].split('_')
+    dive_ids.append(dive[1])
+    paths2jsons.append('/Users/atlanticindianocean/Desktop/scratch/mgds/work/Falkor/'+cruise_id+'/Subastian/'+cruise_id+'_'+dive_ids[i]+'/Sealog/'+dive_ids[i]+'_loweringRecord.json')
+print(paths2jsons)
+
+#print('hello world {}'.format('emily'))
